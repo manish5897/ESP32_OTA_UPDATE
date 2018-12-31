d03a41f9afd449c8da7cc71d11ecfbdc21f7c2d2
Manish <manish@parkzap.com>
2018-12-31 14:37:17 +0530

    initial commit

---
+++ build/cxx/cxx_exception_stubs.d
@@ -0,0 +1,6 @@
+cxx_exception_stubs.o: \
+ /home/manish/esp/esp-idf/components/cxx/cxx_exception_stubs.cpp \
+ /home/manish/github_repo/ESP32_OTA_UPDATE/build/include/sdkconfig.h
+
+/home/manish/github_repo/ESP32_OTA_UPDATE/build/include/sdkconfig.h:
+/home/manish/esp/esp-idf/components/cxx/./cxx_exception_stubs.cpp:
